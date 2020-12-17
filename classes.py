19a5d18a07dc8f178955453b29e5a11fd99fa7e2
Anastasiia Sokolova <anastasiya.sokolov@yandex.ru>
2020-12-17 23:21:13 +0300

    реализация ООП

--- classes.py
+++ classes.py
@@ -1,12 +1,57 @@
+import pymysql
+from pymysql.cursors import DictCursor
+import telebot
+from telebot import *
 
-class MainMenu:
-    def start(self):
-        pass
+import config
+import datetime
 
 
-class Information:
-    def get_information(self):
-        pass
+class TelegramBot:
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='',
+            db='bot',
+            charset='utf8mb4',
+            cursorclass=DictCursor
+        )
+
+        self.bot = telebot.TeleBot(config.token)
+        self.d_today = datetime.date.today()
+        self.d_tomorrow = self.d_today + datetime.timedelta(days=1)
+        self.d_day_after_tom = self.d_today + datetime.timedelta(days=2)
+
+
+class MainMenu(TelegramBot):
+    def __init__(self):
+        self.markup = types.ReplyKeyboardMarkup(row_width=2)
+        info_btn = types.KeyboardButton('Информация')
+        mail_btn = types.KeyboardButton('Рассылка')
+        menu_btn = types.KeyboardButton('Меню')
+        book_btn = types.KeyboardButton('Бронирование')
+        game_btn = types.KeyboardButton('Игра')
+        self.markup.add(info_btn, mail_btn, menu_btn, book_btn, game_btn)
+
+    def start(self, chat_id):
+        with self.connection.cursor() as cursor:
+            user = f"SELECT chat_id FROM chat WHERE chat_id = {chat_id}"
+            cursor.execute(user)
+            if not ([row for row in cursor]):
+                add_user = f"INSERT INTO chat (chat_id) VALUE ({chat_id})"
+                cursor.execute(add_user)
+                self.connection.commit()
+            self.bot.send_message(chat_id, 'Добро пожаловать!\n', reply_markup=self.markup)
+
+
+class Information(TelegramBot):
+    def get_information(self, chat_id):
+        self.bot.send_message(chat_id, 'Время работы:\nПн-Пт: 08:00 - 01:00\nСб-Вс: 09:00 - 02:00\n'
+                                          'Адрес: ул. Политехническая, 29\n'
+                                          'Телефон: 8-800-555-35-35\n'
+                                          'Сайт: www.loveyousomatcha.ru')
+        self.bot.send_location(chat_id, latitude=60.00729003, longitude=30.37286282)
 
 
 class Newsletter:
@@ -17,26 +62,79 @@ class Newsletter:
         pass
 
 
-class Menu:
-    def get_menu(self):
-        pass
+class Menu(TelegramBot):
+    def get_menu(self, chat_id):
+        markup_menu = types.ReplyKeyboardMarkup(row_width=1)
+        main_menu = types.KeyboardButton('Назад')
+        main_dishes = types.KeyboardButton('Основные блюда')
+        dishes = types.KeyboardButton('Закуски')
+        drinks = types.KeyboardButton('Напитки')
+        markup_menu.add(main_dishes, dishes, drinks, main_menu)
+        self.bot.send_message(chat_id, 'Выберите категорию\n', reply_markup=markup_menu)
 
 
-class Booking:
-    def show_map(self):
-        pass
+class Booking(TelegramBot):
+    def show_map(self, chat_id):
+        img = open('static/plancafe.JPG', 'rb')
+        self.bot.send_message(chat_id, 'Карта нашего заведения\n')
+        self.bot.send_photo(chat_id, img)
 
-    def show_dates(self):
-        pass
+    def show_dates(self, chat_id):
+        markup_date = types.ReplyKeyboardMarkup(row_width=1)
+        today = types.KeyboardButton(str(self.d_today))
+        tomorrow = types.KeyboardButton(str(self.d_tomorrow))
+        day_after_tomorrow = types.KeyboardButton(str(self.d_day_after_tom))
+        main_menu = types.KeyboardButton('Главное меню')
+        markup_date.add(today, tomorrow, day_after_tomorrow, main_menu)
+        self.bot.send_message(chat_id, 'Выберите день\n', reply_markup=markup_date)
 
-    def show_times(self):
-        pass
+    def show_times(self, chat_id):
+        markup = types.ReplyKeyboardMarkup(row_width=2)
+        time_18 = types.KeyboardButton('18:00')
+        time_19 = types.KeyboardButton('19:00')
+        time_20 = types.KeyboardButton('20:00')
+        time_21 = types.KeyboardButton('21:00')
+        time_22 = types.KeyboardButton('22:00')
+        time_23 = types.KeyboardButton('23:00')
+        main_menu = types.KeyboardButton('Главное меню')
+        markup.add(time_18, time_19, time_20, time_21, time_22, time_23, main_menu)
+        self.bot.send_message(chat_id, 'Выберите время\n', reply_markup=markup)
 
-    def show_free_tables(self):
-        pass
+    def show_free_tables(self, date_time, chat_id):
+        tables = [n for n in range(1, 30)]
+        with self.connection.cursor() as cursor:
+            sql = "SELECT reserve.table FROM reserve WHERE reserve.date=%s; "
+            cursor.execute(sql, str(date_time))
 
-    def do_reservation(self):
-        pass
+            not_free = []
+            for row in cursor:
+                not_free.append(row['table'])
+        free_tables = [n for n in tables if n not in not_free]
+
+        if free_tables:
+            self.bot.send_message(chat_id, 'Выберите свободный стол\n')
+            self.bot.send_message(chat_id, str(free_tables)[1:-1])
+        else:
+            self.bot.send_message(chat_id, 'К сожалению, мест нет\n')
+
+        return free_tables
+
+    def do_reservation(self, date_time, message, markup, chat_id):
+
+        with self.connection.cursor() as cursor:
+            sql = "SELECT reserve.table FROM reserve WHERE reserve.date=%s; "
+            cursor.execute(sql, str(date_time))
+
+            not_free = []
+            for row in cursor:
+                not_free.append(row['table'])
+            if message.text in list(map(str, not_free)):
+                self.bot.send_message(chat_id, 'К сожалению, ваш столик уже кто-то забронировал, выберите другой.\n')
+            else:
+                add_booking = f"INSERT INTO reserve (reserve.table, reserve.date) VALUES ({int(message.text)}, %s)"
+                cursor.execute(add_booking, str(date_time))
+                self.connection.commit()
+                self.bot.send_message(chat_id, 'Бронь успешно завершена.\n', reply_markup=markup)
 
 
 class Game:
--- main.py
+++ main.py
@@ -1,44 +1,21 @@
-import pymysql
-from pymysql.cursors import DictCursor
-from contextlib import closing
 import datetime
 
-import telebot
-from telebot import *
-import config
-
-connection = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='',
-    db='bot',
-    charset='utf8mb4',
-    cursorclass=DictCursor
-)
+from classes import TelegramBot, Menu, MainMenu, Game, Booking, Information
 
 
-bot = telebot.TeleBot(config.token)
+telegram_bot = TelegramBot()
+menu_bot = Menu()
+main_menu_bot = MainMenu()
+booking_bot = Booking()
+information_bot = Information()
 
-'''Главное меню'''
-markup = types.ReplyKeyboardMarkup(row_width=2)
-info_btn = types.KeyboardButton('Информация')
-mail_btn = types.KeyboardButton('Рассылка')
-menu_btn = types.KeyboardButton('Меню')
-book_btn = types.KeyboardButton('Бронирование')
-game_btn = types.KeyboardButton('Игра')
-markup.add(info_btn, mail_btn, menu_btn, book_btn, game_btn)
-
-d_today = datetime.date.today()
-d_tomorrow = d_today + datetime.timedelta(days=1)
-d_day_after_tom = d_today + datetime.timedelta(days=2)
-booking_date = datetime.date.today()
-free_tables = []
-date_time = 0
+bot = telegram_bot.bot
+booking_date, date_time, free_tables = (0, 0, [])
 
 
 @bot.message_handler(commands=['start'])
 def handle_start_help(message):
-    bot.send_message(message.chat.id, 'Добро пожаловать!\n', reply_markup=markup)
+    main_menu_bot.start(message.chat.id)
 
 
 @bot.message_handler(content_types=['text'])
@@ -46,25 +23,23 @@ def mess(message):
     global booking_date
     global free_tables
     global date_time
-
+    '''
+    Информация
+    '''
     if message.text == 'Информация':
-        bot.send_message(message.chat.id, 'Время работы:\nПн-Пт: 08:00 - 01:00\nСб-Вс: 09:00 - 02:00\n'
-                                          'Адрес: ул. Политехническая, 29\n'
-                                          'Телефон: 8-800-555-35-35\n'
-                                          'Сайт: www.loveyousomatcha.ru')
-        bot.send_location(message.chat.id, latitude=60.00729003, longitude=30.37286282)
+        information_bot.get_information(message.chat.id)
 
+    '''
+    Рассылка
+    '''
     if message.text == 'Рассылка':
         pass
 
+    '''
+    Меню
+    '''
     if message.text == 'Меню':
-        markup_menu = types.ReplyKeyboardMarkup(row_width=1)
-        main_menu = types.KeyboardButton('Главное меню')
-        main_dishes = types.KeyboardButton('Основные блюда')
-        dishes = types.KeyboardButton('Закуски')
-        drinks = types.KeyboardButton('Напитки')
-        markup_menu.add(main_dishes, dishes, drinks, main_menu)
-        bot.send_message(message.chat.id, 'Выберите категорию\n', reply_markup=markup_menu)
+        menu_bot.get_menu(message.chat.id)
 
     if message.text == 'Основные блюда':
         with open('static/menu_1.pdf', 'rb') as file:
@@ -78,36 +53,23 @@ def mess(message):
         with open('static/menu_3.pdf', 'rb') as file:
             bot.send_document(message.chat.id, file)
 
-    '''Блок бронирования'''
+    '''
+    Бронирование
+    '''
     if message.text == "Бронирование":
-        img = open('static/plancafe.JPG', 'rb')
+        booking_bot.show_map(message.chat.id)
+        booking_bot.show_dates(message.chat.id)
 
-        bot.send_message(message.chat.id, 'Карта нашего заведения\n')
-        bot.send_photo(message.chat.id, img)
-        markup2 = types.ReplyKeyboardMarkup(row_width=1)
-        today = types.KeyboardButton(str(d_today))
-        tom = types.KeyboardButton(str(d_tomorrow))
-        day_after_tom = types.KeyboardButton(str(d_day_after_tom))
-        main_menu = types.KeyboardButton('Главное меню')
-        markup2.add(today, tom, day_after_tom, main_menu)
-        bot.send_message(message.chat.id, 'Выберите день\n', reply_markup=markup2)
+    if message.text in ['Назад', 'Главное меню']:
+        bot.send_message(message.chat.id, 'Выберите новое действие\n',
+                         reply_markup=main_menu_bot.markup)
 
-    if message.text == 'Главное меню':
-        bot.send_message(message.chat.id, 'Выберите новое действие\n', reply_markup=markup)
-
-    if message.text in [str(d_today), str(d_tomorrow), str(d_day_after_tom)]:
+    if message.text in [str(telegram_bot.d_today),
+                        str(telegram_bot.d_tomorrow),
+                        str(telegram_bot.d_day_after_tom)]:
 
         booking_date = message.text
-        markup3 = types.ReplyKeyboardMarkup(row_width=2)
-        time_18 = types.KeyboardButton('18:00')
-        time_19 = types.KeyboardButton('19:00')
-        time_20 = types.KeyboardButton('20:00')
-        time_21 = types.KeyboardButton('21:00')
-        time_22 = types.KeyboardButton('22:00')
-        time_23 = types.KeyboardButton('23:00')
-        main_menu = types.KeyboardButton('Главное меню')
-        markup3.add(time_18, time_19, time_20, time_21, time_22, time_23, main_menu)
-        bot.send_message(message.chat.id, 'Выберите время\n', reply_markup=markup3)
+        booking_bot.show_times(message.chat.id)
 
     if message.text in ['18:00', '19:00', '20:00', '21:00', '22:00', '23:00']:
         time = message.text
@@ -116,40 +78,18 @@ def mess(message):
         day = int(booking_date[8:])
         date_time = datetime.datetime(year, mounth, day, int(time[:2]))
 
-        bot.send_message(message.chat.id, 'Выберите свободный стол\n')
-
-        tables = [n for n in range(1, 30)]
-        with connection.cursor() as cursor:
-            sql = "SELECT reserve.table FROM reserve WHERE reserve.date=%s; "
-            cursor.execute(sql, str(date_time))
-
-            not_free = []
-            for row in cursor:
-                not_free.append(row['table'])
-        free_tables = [n for n in tables if n not in not_free]
-        bot.send_message(message.chat.id, str(free_tables)[1:-1])
+        free_tables = booking_bot.show_free_tables(date_time, message.chat.id)
 
     if message.text in list(map(str, free_tables)):
-        with connection.cursor() as cursor:
-            sql = "SELECT reserve.table FROM reserve WHERE reserve.date=%s; "
-            cursor.execute(sql, str(date_time))
+        booking_bot.do_reservation(date_time, message,
+                                   main_menu_bot.markup, message.chat.id)
 
-            not_free = []
-            for row in cursor:
-                not_free.append(row['table'])
-            if message.text in list(map(str, not_free)):
-                bot.send_message(message.chat.id, 'К сожалению, ваш столик уже кто-то забронировал, выберите другой.\n')
-            else:
-                add_booking = f"INSERT INTO reserve (reserve.table, reserve.date) VALUES ({int(message.text)}, %s)"
-                cursor.execute(add_booking, str(date_time))
-                bot.send_message(message.chat.id, 'Бронь успешно завершена.\n', reply_markup=markup)
-
+    '''
+    Игра
+    '''
     if message.text == 'Игра':
         pass
 
-    connection.commit()
-
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
-     connection.close()
+    bot.polling(none_stop=True)
