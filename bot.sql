1244697ead28ba7e784c98f3c8d9828b8a44aeeb
Anne <adudina99@gmail.com>
2020-12-21 18:38:28 +0300

    Game-004. User check

--- bot.sql
+++ bot.sql
@@ -82,7 +82,26 @@ CREATE TABLE `reserve` (
 --
 -- Dumping data for table `reserve`
 --
-
+/*!50106 SET @save_time_zone= @@TIME_ZONE */ ;
+/*!50106 DROP EVENT IF EXISTS `game_check` */;
+DELIMITER ;;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;;
+/*!50003 SET character_set_client  = utf8mb4 */ ;;
+/*!50003 SET character_set_results = utf8mb4 */ ;;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;;
+/*!50003 SET @saved_time_zone      = @@time_zone */ ;;
+/*!50003 SET time_zone             = 'SYSTEM' */ ;;
+/*!50106 CREATE*/ /*!50117 DEFINER=`root`@`localhost`*/ /*!50106 EVENT `game_check` ON SCHEDULE EVERY 1 MONTH STARTS '2020-12-21 15:55:00' ON COMPLETION NOT PRESERVE ENABLE COMMENT 'reset table' DO truncate game */ ;;
+/*!50003 SET time_zone             = @saved_time_zone */ ;;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;;
+/*!50003 SET character_set_results = @saved_cs_results */ ;;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;;
+DELIMITER ;
 LOCK TABLES `reserve` WRITE;
 /*!40000 ALTER TABLE `reserve` DISABLE KEYS */;
 /*!40000 ALTER TABLE `reserve` ENABLE KEYS */;
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import datetime
-import schedule
-import time
+
+import pymysql
+from pymysql.cursors import DictCursor
+
 from classes import TelegramBot, Menu, MainMenu, Game, Booking, Information, Newsletter
 from random import shuffle
+import schedule
+import time
+from telebot import *
 from multiprocessing.context import Process
+
 
 
 telegram_bot = TelegramBot()
@@ -15,15 +21,14 @@ newsletter = Newsletter()
 
 game_bot = Game()
 qNum = 0
-correct_answer = ''
+correctAnsw = ''
 question = ''
 score = 0
 isGame = False
-isBooking = False
 
 
 bot = telegram_bot.bot
-booking_date, date_time, free_tables, questions = (0, 0, [], 0)
+booking_date, date_time, free_tables = (0, 0, [])
 
 
 @bot.message_handler(commands=['start'])
@@ -39,38 +44,81 @@ def mess(message):
     global questions
     global qNum
     global score
-    global correct_answer
+    global correctAnsw
     global question
     global isGame
-    global isBooking
+
 
     if message.text == 'Информация':
-        isGame, isBooking = False, False
         information_bot.get_information(message.chat.id)
 
-    elif message.text == 'Начать':
-        isGame = True
-        qNum = 0
-        score = 0
-        questions = game_bot.get_questions()
-
-        correct_answer, question = do_question(questions[qNum])
-        print_question(message.chat.id, question)
-
-    elif message.text in ['1', '2', '3', '4', 'пропустить'] and isGame:
-        qNum = qNum + 1
-        if qNum < 10:
-            score = score + game_bot.start_game(message, correct_answer, question, qNum)
-            correct_answer, question = do_question(questions[qNum])
-            print_question(message.chat.id, question)
-        else:
-            game_bot.check_result(message.chat.id, score)
-            questions.clear()
-            isGame = False
-
     elif message.text == 'Рассылка':
-        isGame, isBooking = False, False
-        newsletter.check_subscription(message.chat.id)
+       newsletter.check_subscription(message.chat.id)
+
+    elif message.text == 'Подписаться на рассылку':
+       newsletter.change_subscription(message.chat.id)
+
+    elif message.text == 'Начать':
+        if not isGame:
+           connection = pymysql.connect(
+                host='localhost',
+                user='root',
+                password='',
+                db='bot',
+                charset='utf8mb4',
+                cursorclass=DictCursor
+           )
+           with connection.cursor() as cursor:
+               query = f"SELECT * FROM game WHERE chat_id = {message.chat.id};"
+               cursor.execute(query)
+               result = cursor.fetchone()
+               connection.commit()
+               if not result:
+                   isGame = True
+                   qNum = 0
+                   score = 0
+                   questions = game_bot.get_questions()
+                   now = datetime.datetime.now()
+                   with connection.cursor() as cursor:
+                       add_user = f"INSERT INTO game (game.chat_id, game.last_game) VALUES ({int(message.chat.id)}, %s);"
+                       connection.begin
+                       cursor.execute(add_user, str(now))
+                       connection.commit()
+                       connection.close()
+
+                   correctAnsw, question = do_question(questions[qNum])
+                   print_question(message.chat.id, question)
+
+               else:
+                   markup_game = types.ReplyKeyboardMarkup(row_width=1)
+                   main_menu = types.KeyboardButton('Назад')
+                   markup_game.add(main_menu)
+                   bot.send_message(message.chat.id,
+                                         'Ты пока не можешь сыграть еще раз, попробуй поиграть еще раз в'
+                                         ' следующем календарном месяце\n\n',
+                                         reply_markup=markup_game)
+
+
+        else:
+            bot.send_message(message.chat.id, 'Вы уже начали игру\n',
+                             reply_markup=game_bot.markup)
+
+
+
+
+    elif message.text in ['1', '2', '3', '4', 'пропустить']:
+        if isGame:
+            qNum = qNum + 1
+            if qNum<10:
+                score = score + game_bot.start_game(message, correctAnsw, question, qNum)
+                correctAnsw, question = do_question(questions[qNum])
+                print_question(message.chat.id, question)
+            else:
+                game_bot.check_result(message.chat.id, score)
+                questions.clear()
+                isGame = False
+        else:
+             newsletter.check_subscription(message.chat.id)
 
     elif message.text == 'Подписаться на рассылку':
         newsletter.change_subscription(message.chat.id)
@@ -79,7 +127,6 @@ def mess(message):
         newsletter.change_subscription(message.chat.id)
 
     elif message.text == 'Меню':
-        isGame, isBooking = False, False
         menu_bot.get_menu(message.chat.id)
 
     elif message.text == 'Основные блюда':
@@ -95,18 +142,16 @@ def mess(message):
             bot.send_document(message.chat.id, file)
 
     elif message.text == "Бронирование":
-        isGame, isBooking = False, False
         booking_bot.show_map(message.chat.id)
         booking_bot.show_dates(message.chat.id)
 
     elif message.text in ['Назад', 'Главное меню']:
-        isGame, isBooking = False, False
         bot.send_message(message.chat.id, 'Выберите новое действие\n',
                          reply_markup=main_menu_bot.markup)
 
     elif message.text in [str(telegram_bot.d_today),
-                          str(telegram_bot.d_tomorrow),
-                          str(telegram_bot.d_day_after_tom)]:
+                        str(telegram_bot.d_tomorrow),
+                        str(telegram_bot.d_day_after_tom)]:
 
         booking_date = message.text
         booking_bot.show_times(message.chat.id)
@@ -118,50 +163,51 @@ def mess(message):
                                       int(message.text[:2]))
 
         free_tables = booking_bot.show_free_tables(date_time, message.chat.id)
-        isBooking = True
 
     elif message.text in list(map(str, free_tables)):
         booking_bot.do_reservation(date_time, message,
                                    main_menu_bot.markup, message.chat.id)
 
     elif message.text == 'Игра':
-        isBooking = False
+        isGame = False
         game_bot.check_user(message.chat.id)
 
-    elif isGame:
-        bot.send_message(message.chat.id,
-                         "Упс! Ты ввел(а) некорректный ответ. "
-                         "Попробуй еще раз либо отправь “пропустить” "
-                         "и перейдешь к следующему вопросу”")
-    elif isBooking:
-        bot.send_message(message.chat.id,
-                         'Вы ввели некорректный номер либо этот '
-                         'столик уже занят, выберите другой.\n')
     else:
-        bot.send_message(message.chat.id, 'Нажмите кнопку!\n')
+         if isGame:
+            bot.send_message(message.chat.id,
+                          "Упс! Ты ввел(а) некорректный ответ. Попробуй еще раз либо отправь “пропустить” и перейдешь к следующему вопросу”")
+         else:
+             try:
+                 int(message.text)
+                 bot.send_message(message.chat.id,
+                                  'Вы ввели некорректный номер либо этот '
+                                  'столик уже занят, выберите другой.\n')
+             except ValueError:
+                 bot.send_message(message.chat.id, 'Нажмите кнопку!\n')
 
 
-def do_question(some_question):
-    cor_answer = some_question["answers"][0]
-    shuffle(some_question["answers"])
-    return cor_answer, some_question
+def do_question(question):
+    correctAnsw = question["answers"][0]
+    shuffle(question["answers"])
+    return correctAnsw, question
 
-
-def print_question(chat_id, some_question):
-    bot.send_message(chat_id, f'\n{some_question["question"]}\n')
+def print_question(chat_id, question):
+    bot.send_message(chat_id, f'\n{question["question"]}\n')
     numbering = "12345"
     i = 0
-    for answer in some_question["answers"]:
+    for answer in question["answers"]:
         bot.send_message(chat_id, numbering[i] + ". " + answer, "\n")
         i += 1
         pass
 
-# для проверки работы рассылки сообщения отправляются каждую минуту
-# schedule.every().minute.at(":17").do(newsletter.send_newsletter)
 
+
+
+# для проверки работы рассылки сообщения отправляются каждую минуту
+schedule.every().minute.at(":17").do(newsletter.send_newsletter)
 
 # можно, к примеру, отправлять рассылку каждый понедельник в 12:00
-schedule.every().monday.at("12:00").do(newsletter.send_newsletter)
+# schedule.every().monday.at("12:00").do(newsletter.send_newsletter)
 
 
 class ScheduleMessage:
